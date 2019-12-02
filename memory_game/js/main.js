e2321ce61c5611f7f9dabaa2229f40ab7765e3d5
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2019-12-01 20:22:19 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardThree)
+var cards = ['queen', 'queen', 'king', 'king',];
+console.log("User flipped" + " " +cards[0])
+console.log("User flipped" + " " +cards[2])
+var cardsInPlay = []
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+var cardOne = cards[0];
+var cardTwo = cards[2];
+if (cardsInPlay.length === 0) {
+}	else if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert("Sorry try again!")
+}
+
